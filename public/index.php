982ec63776b4f05dc806399219f38805393324b0
ruben arias <ruprosperi2@gmail.com>
2021-05-05 19:05:05 -0400

    se crearon las clases arqueros y soldado

--- public/index.php
+++ public/index.php
@@ -1,2 +1,11 @@
 <?php
 
+namespace Curso;
+
+require_once "../vendor/autoload.php";
+
+$ruben = new Soldier("ruben");
+$jesus = new Archer("jesus");
+
+$ruben->move("norte");
+$jesus->move("sur");
---
+++ src/Archer.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Curso;
+
+class Archer extends Unit
+{
+    
+}
